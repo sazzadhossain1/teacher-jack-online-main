287b4899494b10aa3c72e709bbb6340da202cdf7
sazzadhossain1 <sazzadtomal1@gmail.com>
2024-05-16 18:41:46 +0600

    add carousel

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,48 @@
+import React from "react";
+// import React from "react";
+import "./Carousel.css";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import required modules
+import { EffectCube, Pagination } from "swiper/modules";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
+
+import certificate from "../../accets/TeacherJackPhoto/certificate.jpg";
+import teacherJackPhoto from "../../accets/TeacherJackPhoto/teacherJack.png";
+
+const Carousel = () => {
+  return (
+    <div>
+      <>
+        <Swiper
+          effect={"cube"}
+          grabCursor={true}
+          cubeEffect={{
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 20,
+            shadowScale: 0.94,
+          }}
+          pagination={true}
+          modules={[EffectCube, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src={certificate} alt="Certificate" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={teacherJackPhoto} alt="Teacher_Jack_Photo" />
+          </SwiperSlide>
+        </Swiper>
+      </>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Home.css";
 
+import Carousel from "../Carousel/Carousel";
+
 const Home = () => {
   return (
     <div>
       <h1>This is Home</h1>
+      <Carousel></Carousel>
     </div>
   );
 };
